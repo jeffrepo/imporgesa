fc12be36b401d5b520791f5d5b2f54a4ceb872ba
jeffrepo <jeffer.leo93@gmail.com>
2022-03-31 22:51:13 -0600

    Campo marca

---
+++ __manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Imporgesa",
+
+    'summary': """ Imporgesa """,
+
+    'description': """
+        Imporgesa
+    """,
+
+    'author': "STECHNOLOGIES",
+    'website': "",
+
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    'depends': ['product'],
+
+    'data': [
+        'views/product_template_views.xml',
+    ],
+    'qweb': [
+    ],
+    'license': 'LGPL-3',
+}
