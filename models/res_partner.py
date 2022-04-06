891b30b7a15184eb971807205e31b215fde7580d
jeffrepo <jeffer.leo93@gmail.com>
2022-04-06 00:06:22 -0600

    update

--- models/res_partner.py
+++ models/res_partner.py
@@ -6,6 +6,6 @@ from odoo import api, fields, models, tools, _
 class ProductTemplate(models.Model):
     _inherit = "res.partner"
 
-    giro__negocio = fields.Selection([('1', 'Publico final'),('2', 'Mueblería'), ('3', 'Corporativo'), ('4', 'Educación'),
-    ('5', 'Gobierno'), ('6', 'Eventos'), ('7', 'Distribuidora'), ('8', 'Restaurante'), ('9', 'Iglesia'), ('10', 'Hotelería'),
-    ('11', 'Vendedor al detalle')])
+    giro_negocio = fields.Selection([('publico_final', 'Publico final'),('muebleria', 'Mueblería'), ('corporativo', 'Corporativo'), ('educacion', 'Educación'),
+    ('gobierno', 'Gobierno'), ('eventos', 'Eventos'), ('distribuidora', 'Distribuidora'), ('retaurante', 'Restaurante'), ('iglesia', 'Iglesia'), ('hoteleria', 'Hotelería'),
+    ('vendedor_detalle', 'Vendedor al detalle')])
