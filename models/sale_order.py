bf7d87fbb238d78cbf13300b26a77802efebb9c9
jeffrepo <jeffer.leo93@gmail.com>
2022-04-26 23:47:40 -0600

    no vender de mas

---
+++ models/sale_order.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from odoo import api, fields, models, SUPERUSER_ID, _
+from odoo.exceptions import AccessError, UserError, ValidationError
+
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+
+    def action_confirm(self):
+        for sale in self:
+            product_zero = False
+            list_product = []
+            if sale.order_line:
+                for line in sale.order_line:
+                    if line.qty_available_today < line.product_uom_qty:
+                        product_zero = True
+                        list_product.append(linea.product_id.name)
+
+            if product_zero and len(list_product) > 0:
+                raise UserError(_(
+                    'Productos sin existencia' + str(list_product)))
+        return super(SaleOrder, self).action_confirm()
